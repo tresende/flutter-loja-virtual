a2c93ffe254dab2bf5746528a6383dd88ebdec3b
tresende <thiago.gcresende@gmail.com>
2019-04-28 18:12:42 -0300

    exibindo images

--- lib/Tabs/home_tab.dart
+++ lib/Tabs/home_tab.dart
@@ -1,5 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 class HomeTab extends StatelessWidget {
   @override
@@ -47,12 +49,24 @@ class HomeTab extends StatelessWidget {
                                 AlwaysStoppedAnimation<Color>(Colors.white),
                           )));
                 } else {
-                  print(snapshot.data.documents.length);
-                  return SliverToBoxAdapter(
-                      child: Container(
-                          height: 200,
-                          alignment: Alignment.center,
-                          child: Container()));
+                  return SliverStaggeredGrid.count(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 1,
+                    crossAxisSpacing: 1,
+                    staggeredTiles: snapshot.data.documents.map((doc) {
+                      return StaggeredTile.count(
+                        doc.data["x"],
+                        doc.data["y"],
+                      );
+                    }).toList(),
+                    children: snapshot.data.documents.map((doc) {
+                      return FadeInImage.memoryNetwork(
+                        placeholder: kTransparentImage,
+                        image: doc.data["image"],
+                        fit: BoxFit.cover,
+                      );
+                    }).toList(),
+                  );
                 }
               },
             )
