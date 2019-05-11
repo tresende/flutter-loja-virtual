c6557ac99dd7a8af3f6f26b334e22f15a9639ef0
tresende <thiago.gcresende@gmail.com>
2019-05-11 19:46:07 -0300

    Estrutura de produtos

---
+++ lib/models/product.dart
@@ -0,0 +1,20 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Product {
+  String id;
+  String title;
+  String description;
+  double price;
+  List images;
+  List sizes;
+  String category;
+
+  Product.fromDocument(DocumentSnapshot snapshot) {
+    this.id = snapshot.documentID;
+    this.title = snapshot.data['title'];
+    this.description = snapshot.data['description'];
+    this.price = snapshot.data['price'];
+    this.images = snapshot.data['images'];
+    this.sizes = snapshot.data['sizes'];
+  }
+}
