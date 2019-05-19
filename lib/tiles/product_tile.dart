34d580af119e921796e184931e945888ef6b30ba
tresende <thiago.gcresende@gmail.com>
2019-05-19 15:15:45 -0300

    exibindo os produtos no grid

--- lib/tiles/product_tile.dart
+++ lib/tiles/product_tile.dart
@@ -2,14 +2,48 @@ import 'package:flutter/material.dart';
 import 'package:flutter_loja_virtual/models/product.dart';
 
 class ProductTile extends StatelessWidget {
-
-
   final String type;
-  final Product data;
-  ProductTile(this.type, this.data);
+  final Product product;
+  ProductTile(this.type, this.product);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return InkWell(
+        child: Card(
+            child: this.type == "grid"
+                ? Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      AspectRatio(
+                        aspectRatio: 0.8,
+                        child: Image.network(
+                          this.product.images[0],
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      Expanded(
+                        child: Container(
+                          padding: EdgeInsets.all(8),
+                          child: Column(
+                            children: <Widget>[
+                              Text(
+                                product.title,
+                                style: TextStyle(fontWeight: FontWeight.w500),
+                              ),
+                              Text(
+                                "R\$ ${product.price.toStringAsFixed(2)}",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 17,
+                                    color: Theme.of(context).primaryColor),
+                              ),
+                            ],
+                          ),
+                        ),
+                      )
+                    ],
+                  )
+                : Row()));
   }
 }
