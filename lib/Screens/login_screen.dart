0951b0f4d68c46e50d161f865e5ffa649d0899e4
Thiago Resende <thiago.gcresende@gmail.com>
2019-06-24 22:31:08 -0300

    remocao import

--- lib/Screens/login_screen.dart
+++ lib/Screens/login_screen.dart
@@ -4,8 +4,6 @@ import 'package:flutter_loja_virtual/Screens/signup_screen.dart';
 import 'package:flutter_loja_virtual/models/user_model.dart';
 import 'package:scoped_model/scoped_model.dart';
 
-import 'home_screen.dart';
-
 class LoginScreen extends StatefulWidget {
   @override
   _LoginScreenState createState() => _LoginScreenState();
