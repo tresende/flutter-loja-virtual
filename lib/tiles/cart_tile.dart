835dde97d02b61c3f761fe87310281cfa126d343
Thiago Resende <thiago.gcresende@gmail.com>
2019-08-25 10:26:18 -0300

    Mostrando Item no Carrinho

--- lib/tiles/cart_tile.dart
+++ lib/tiles/cart_tile.dart
@@ -8,10 +8,69 @@ class CartTile extends StatelessWidget {
 
   CartTile(this.cartProduct);
 
-  Widget _buildContent() {}
-
   @override
   Widget build(BuildContext context) {
+    Widget _buildContent() {
+      return Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.all(8),
+            width: 120,
+            child: Image.network(cartProduct.productData.images[0],
+                fit: BoxFit.cover),
+          ),
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.all(8),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(
+                    cartProduct.productData.title,
+                    style: TextStyle(fontSize: 17, fontWeight: FontWeight.w500),
+                  ),
+                  Text(
+                    "Tamanho ${cartProduct.size}",
+                    style: TextStyle(fontWeight: FontWeight.w300),
+                  ),
+                  Text(
+                    "R\$ ${cartProduct.productData.price.toStringAsFixed(2)}",
+                    style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: Theme.of(context).primaryColor),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      IconButton(
+                        color: Theme.of(context).primaryColor,
+                        icon: Icon(Icons.remove),
+                        onPressed: cartProduct.quantity > 1 ? () {} : null,
+                      ),
+                      Text(cartProduct.quantity.toString()),
+                      IconButton(
+                        color: Theme.of(context).primaryColor,
+                        icon: Icon(Icons.add),
+                        onPressed: () {},
+                      ),
+                      FlatButton(
+                        child: Text("Remover"),
+                        textColor: Colors.grey,
+                        onPressed: () {},
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      );
+    }
+
     return Card(
         margin: EdgeInsets.symmetric(horizontal: 4, vertical: 8),
         child: cartProduct.productData == null
